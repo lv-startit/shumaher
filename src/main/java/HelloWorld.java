8b2cb76dc6beebabe11ba0d1d9653f6b1a9ed383
Valentin Gaiduchek <ukrainis.v@gmail.com>
2015-04-13 15:28:45 +0300

    Added MaxSpeed info

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,6 +4,8 @@ import lejos.hardware.BrickFinder;
 import lejos.hardware.Keys;
 import lejos.hardware.ev3.EV3;
 import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.motor.Motor;
+import lejos.robotics.RegulatedMotor;
 
 
 /**
@@ -18,6 +20,11 @@ public class HelloWorld {
 		Keys keys = ev3.getKeys();
 		
 		lcd.drawString("Hello World", 4, 4);
+		keys.waitForAnyPress();
+		
+		RegulatedMotor moto = Motor.A;
+		float maxSpeed = moto.getMaxSpeed();
+		lcd.drawString("Max speed: " + maxSpeed, 4, 4);
 		keys.waitForAnyPress();
 	}
 	
